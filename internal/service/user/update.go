f8755a64351770d963e174e242a44ffe4395e9b0
zd4rova <fakenotbad75@gmail.com>
2023-05-17 15:13:52 +0300

    update: implement username usage

--- internal/service/user/update.go
+++ internal/service/user/update.go
@@ -6,11 +6,34 @@ import (
 	model "github.com/zd4r/auth/internal/model/user"
 )
 
-func (s *service) Update(ctx context.Context, user *model.User) error {
-	err := s.userRepository.Update(ctx, user)
+func (s *service) Update(ctx context.Context, username string, user *model.User) error {
+	latestUser, err := s.userRepository.Get(ctx, username)
+	if err != nil {
+		return err
+	}
+
+	match(user, latestUser)
+
+	err = s.userRepository.Update(ctx, username, latestUser)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func match(user, latestUser *model.User) {
+	if user.Username.Valid {
+		latestUser.Username = user.Username
+	}
+
+	if user.Email.Valid {
+		latestUser.Email = user.Email
+	}
+
+	if user.Password.Valid {
+		latestUser.Password = user.Password
+	}
+
+	latestUser.Role = user.Role
 }
